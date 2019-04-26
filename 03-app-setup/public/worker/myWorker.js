b61a093f9f09288527e2b5474510d7cfc6be2d13
Magesh <tkmagesh77@gmail.com>
2019-04-26 14:29:49 +0530

    server sent events

--- 03-app-setup/public/worker/myWorker.js
+++ 03-app-setup/public/worker/myWorker.js
@@ -1,10 +1,14 @@
 function doWork(){
-	for(var i=0;i<10000; i++)
+	for(var i=0;i<10000; i++){
+		var percentCompleted = (i / 10000) * 100;
+		self.postMessage({ type : 'progress', percentCompleted : percentCompleted});
 		for(var j=0; j<10000; j++)
 			for(var k=0; k < 100; k++){
 
 			}
-	
+	}
+	self.postMessage({ type : 'complete'});
+
 }
 
 self.addEventListener('message', onMessageFromMain);
